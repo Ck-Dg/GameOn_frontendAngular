10da2b15c90ce535a5de9f2f78d7f08c01e94efc
CheickD <cheick.dieng@gmail.com>
2022-07-10 08:09:45 +0200

    make it better

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/front-projet4/fr-FR'));
+app.use(express.static(__dirname + '/dist/front-projet4/eng-CA'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/front-projet4/fr-FR/index.html'));
+res.sendFile(path.join(__dirname+'/dist/front-projet4/eng-CA/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
